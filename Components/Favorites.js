d54958d0e52ca8a56700abdb54afc4fb4a74b44a
leo-hardy <40430728+leo-hardy@users.noreply.github.com>
2020-05-27 17:35:05 +0200

    add default avatar

--- Components/Favorites.js
+++ Components/Favorites.js
@@ -4,6 +4,7 @@ import React from 'react'
 import { StyleSheet, View, ActivityIndicator } from 'react-native'
 import { connect } from 'react-redux'
 import FilmList from './FilmList'
+import Avatar from './Avatar'
 
 class Favorites extends React.Component {
 
@@ -26,6 +27,9 @@ class Favorites extends React.Component {
   render() {
     return (
       <View style={styles.main_container}>
+        <View style={styles.avatar_container}>
+          <Avatar/>
+        </View>
         <FilmList
           films={this.props.favoritesFilm} 
           navigation={this.props.navigation}
@@ -41,6 +45,9 @@ const styles = StyleSheet.create({
   main_container: {
     flex: 1,
   },
+  avatar_container: {
+    alignItems: 'center'
+  }
 })
 
 
